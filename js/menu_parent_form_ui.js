072367da060a83078a905e3602df9420cc7a8350
Jim Keller <jimk@easternstandard.com>
2018-07-09 10:15:16 -0400

    fixed duplicate menu choice appears on nodes; added hierarchical parent choosing from menu form as well

--- js/menu_parent_form_ui.js
+++ js/menu_parent_form_ui.js
@@ -9,28 +9,28 @@
   Drupal.behaviors.menu_parent_form_ui = {
     attach: function (context, settings) {
 
-      if ( document.querySelector('#edit-menu-parent') ) {
-        var select_extract_menu_form = new SelectExtractor(
-          {
-            selectors: {
-              select_input_original: '#edit-menu-parent',
-              select_box_container: '.form-item-menu-parent'
+      if ( context == document ) {
+        if ( document.querySelector('#edit-menu-parent') ) {
+          var select_extract_menu_form = new SelectExtractor(
+            {
+              selectors: {
+                select_input_original: '#edit-menu-parent',
+                 select_box_container: '.form-item-menu-parent'
+             }
             }
-          }
-        );
-      }
-      else {
-        var select_extract_node_form = new SelectExtractor(
-          {
-            selectors: {
-              select_input_original: '.menu-parent-select',
-              select_box_container: '.form-item-menu-menu-parent'
+          );
+        }
+        else {
+          var select_extract_node_form = new SelectExtractor(
+            {
+              selectors: {
+                select_input_original: '.menu-parent-select',
+                select_box_container: '.form-item-menu-menu-parent'
+              }
             }
-          }
-        );
+          );
+        }
       }
-
-
 
     }
   };
