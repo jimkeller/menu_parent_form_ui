ecb9aaadc12daa49e258d1f743d282df257bafe2
smustgrave <stephen.mustgrave@mobomo.com>
2020-11-09 09:47:47 -0500

    Update menu_parent_form_ui.js

--- js/menu_parent_form_ui.js
+++ js/menu_parent_form_ui.js
@@ -3,19 +3,21 @@
  * Behavior which initializes the select_extractor library for the menu node form
  */
 
-(function (Drupal) {
+(function (Drupal, DrupalSettings) {
   'use strict';
 
   Drupal.behaviors.menu_parent_form_ui = {
     attach: function (context, settings) {
 
-      if ( context == document ) {
+      if ( context === document ) {
+        var active_set = DrupalSettings.active_set;
         if ( document.querySelector('#edit-menu-parent') ) {
           var select_extract_menu_form = new SelectExtractor(
             {
               selectors: {
                 select_input_original: '#edit-menu-parent',
-                 select_box_container: '.form-item-menu-parent'
+                select_box_container: '.form-item-menu-parent',
+                active_set: active_set,
              }
             }
           );
@@ -25,7 +27,8 @@
             {
               selectors: {
                 select_input_original: '.menu-parent-select',
-                select_box_container: '.form-item-menu-menu-parent'
+                select_box_container: '.form-item-menu-menu-parent',
+                active_set: active_set,
               }
             }
           );
@@ -34,4 +37,4 @@
 
     }
   };
-})(Drupal);
+})(Drupal, drupalSettings);
